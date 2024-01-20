4496fe2cd9cafa18df8845ab2b1e1a9ef05f791d
GilbertKabunyu <gkabunyuk15@gmail.com>
2024-01-20 10:21:32 +0200

    user journal and learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,62 @@
 using System;
 
+
+
 class Program
 {
-    static void Main(string[] args)
+    public string Company {get; set;}
+    public string JobTitle {get; set;}
+    public int StartYear {get; set;}
+    public int EndYear {get; set;}
+
+    public Job(string company, string jobTitle, int startYear, int endYear)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Company = company;
+        JobTitle = jobTitle;
+        StartYear = startYear;
+        EndYear = endYear;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{JobTitle} ({Company}) {StartYear}-{EndYear}");
     }
 }
+
+public class Resume
+{
+    public string Name {get; set;}
+    public List<Job> Jobs {get; set;}
+
+    public Resume(string name)
+    {
+        Name = name;
+        Jobs = new List<Job>();
+    }
+
+    public void AddJobs(Job job)
+    {
+        Jobs.Add(job);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(Name);
+        foreach(Job job in Jobs)
+        {
+            job.Display();
+        }
+    }
+    static void Main(string[] args)
+   {
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022,2023);
+
+        Resume myResume = new Resume("Allison Rose");
+        myResume.AddJobs(job1);
+        myResume.AddJobs(job2);
+
+        myResume.Display();
+   } 
+
+} 
